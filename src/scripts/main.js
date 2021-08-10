618c1623d5dc22a9aa0482d02c6e1ef6c891e8df
Taras <hevko.tars@gmail.com>
2021-08-10 17:49:31 +0300

    implement layout_mia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burger = document.querySelector('.header__burger');
+const links = document.querySelector('.header__nav');
+const button = document.querySelector('.gallery__button-link');
+
+burger.addEventListener('click', () => {
+  links.classList.toggle('header__nav--active');
+  burger.classList.toggle('header__burger--active');
+});
+
+button.addEventListener('click', () => {
+  button.classList.toggle('gallery__button-link--active');
+});
